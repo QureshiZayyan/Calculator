68b0473fd15046b9a00533aba1053d79f2aae698
Zayyan <qureshizayyan49@gmail.com>
2024-02-06 05:12:32 +0530

    make some changes in a code

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ console.log('welcome to MyCalculator')
 
 let string = '';
 const btn = document.querySelectorAll('.btn');
+let cal = document.querySelector('.cal');
 
 Array.from(btn).forEach((button) => {
    button.addEventListener('click', (item) => {
 
       if (item.target.innerHTML == '=') {
          string = eval(string)
-         document.querySelector('.cal').value = string;
+         cal.value = string;
       }
 
       else if (item.target.innerHTML == 'C') {
          string = '';
-         document.querySelector('.cal').value = string;
+         cal.value = string;
       }
 
+      else if (item.target.innerHTML == 'E' || item.target.innerHTML == '+/-') {
+         string = 'undefined';
+         cal.value = string;
+      }
       else {
-         console.log(item.target);
          string = string + item.target.innerHTML;
-         document.querySelector('.cal').value = string;
+         cal.value = string;
       }
    });
 });
